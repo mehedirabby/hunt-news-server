8d4547ea1d1056c40d05cb146fe42b83be2ab959
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-10-24 20:06:16 +0600

    ko

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("this is news");
 });
 
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.params.id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
 app.get("/news_categories", (req, res) => {
   res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
 });
 
 app.listen(port, () => {
